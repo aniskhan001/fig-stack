d2fdcfa96eb58796f8190f002eddbfc5eef6e0b4
Anisuzzaman Khan <aniskhan001@outlook.com>
2020-04-04 09:15:45 +0600

    glueing fluent and influx together

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 set -e
 set -o pipefail
 
-echo "\n\n installing Fluent Bit .."
-helm upgrade --install fig-fluent-bit fluent-bit -n monitoring --dry-run
-
 echo "\n\n installing InfluxDB .."
-helm upgrade --install fig-influxdb influxdb -n monitoring --dry-run
+helm upgrade --install influxdb influxdb -n monitoring --dry-run
+
+echo "\n\n installing Fluent Bit .."
+helm upgrade --install fluent-bit fluent-bit -n monitoring --dry-run
 
 echo "\n\n installing Grafana .."
-helm upgrade --install fig-grafana grafana -n monitoring --dry-run
+helm upgrade --install grafana grafana -n monitoring --dry-run
 
